5d3ad441b2d13425339c4c21632fbd9ef04fa439
Роман <rkaeplive@gmail.com>
2024-05-08 16:22:44 +0300

    add dimentions utils

--- dist/Utils/Utils.d.ts
+++ dist/Utils/Utils.d.ts
@@ -7,15 +7,18 @@ import ClippingUtils from "./ClippingUtils/ClippingUtils";
 import NavigationCube from "./NavigationCube/NavigationCube";
 import ExportUtils from "./ExportUtils";
 import { TDecoder } from "./Utils.types";
+import { DimensionsUtils } from "./DimentionsUtils/DimentionsUtils";
 export default class Utils {
     readonly context: BimatterViewer;
     readonly propsUtils: PropsUtils;
     readonly geometryUtils: GeometryUtils;
     readonly clippingUtils: ClippingUtils;
+    readonly dimentionsUtils: DimensionsUtils;
     readonly navigationCubeUtil: NavigationCube;
     readonly exportUtils: ExportUtils;
     readonly decoder: TDecoder;
     stats: Stats | undefined;
     constructor(context: BimatterViewer);
     set useStats(useStats: boolean);
+    disposeMeshRecursively(mesh: any): void;
 }
