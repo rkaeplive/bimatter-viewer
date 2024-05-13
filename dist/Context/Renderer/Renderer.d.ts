2e928ebdcefdb39bc73911ab1864b6257f83c76f
Роман <rkaeplive@gmail.com>
2024-05-13 14:14:37 +0300

    add screenshot func to renderer

--- dist/Context/Renderer/Renderer.d.ts
+++ dist/Context/Renderer/Renderer.d.ts
@@ -1,4 +1,4 @@
-import { Clock, WebGLRenderer } from "three";
+import { Clock, OrthographicCamera, PerspectiveCamera, WebGLRenderer } from "three";
 import { CSS2DRenderer } from "three/examples/jsm/renderers/CSS2DRenderer.js";
 import { CSS3DRenderer } from "three/examples/jsm/renderers/CSS3DRenderer.js";
 import Context from "../Context";
@@ -7,10 +7,12 @@ export default class Renderer {
     threeRenderer2D: CSS2DRenderer;
     threeRenderer3D: CSS3DRenderer;
     threeRenderer: WebGLRenderer;
+    tempRenderer: WebGLRenderer | undefined;
     private animationCallbackList;
     clock: Clock;
     needUpdate: boolean;
     constructor(context: Context);
+    newScreenshot(camera?: OrthographicCamera | PerspectiveCamera): string;
     resize(): void;
     private animation;
     addCallback(callback: () => any): void;
