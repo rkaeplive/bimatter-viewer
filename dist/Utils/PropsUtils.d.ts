7d50fa1709f74146f0c7d29af0a737551bb4008f
Роман <rkaeplive@gmail.com>
2024-05-14 16:34:01 +0300

    add custon selection function

--- dist/Utils/PropsUtils.d.ts
+++ dist/Utils/PropsUtils.d.ts
@@ -1,12 +1,12 @@
+import { PropData } from "../Model/Model.types";
 import Utils from "./Utils";
 export default class PropsUtils {
     readonly context: Utils;
     propConteiner: HTMLElement | undefined;
     constructor(context: Utils);
-    private _getPropertiesByIdFunction;
-    private _getPropertiesById;
-    get getPropertiesById(): typeof this._getPropertiesById;
-    set getPropertiesById(func: typeof this._getPropertiesById);
+    private _getPropertiesByIdOveride;
+    getPropertiesById(modelID: number, elementId: number, useDefault?: boolean): PropData | undefined;
+    setGetParamsByIdOverride(func: typeof this.getPropertiesById | undefined): void;
     initPropConteiner(conteiner: HTMLElement): void;
     private generatePropsThree;
 }
