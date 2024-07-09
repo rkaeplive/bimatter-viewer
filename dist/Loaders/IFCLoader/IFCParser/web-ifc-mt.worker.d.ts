4eb90d65aff69fb02f2a6bf1ad1eb30033bda127
Роман <rkaeplive@gmail.com>
2024-07-09 14:49:58 +0300

    update ts module to nodenext

--- dist/Loaders/IFCLoader/IFCParser/web-ifc-mt.worker.d.ts
+++ dist/Loaders/IFCLoader/IFCParser/web-ifc-mt.worker.d.ts
@@ -1,8 +1,1 @@
-declare function threadPrintErr(...args: any[]): void;
-declare function threadAlert(...args: any[]): void;
-declare function handleMessage(e: any): void;
-declare namespace Module {
-    function instantiateWasm(info: any, receiveInstance: any): any;
-}
-declare var initializedJS: boolean;
-declare function err(...args: any[]): void;
+export {};
