786f91f0c5944e8f2c1613d537a17a707707effb
Роман <rkaeplive@gmail.com>
2024-08-30 15:48:14 +0300

    worker update

--- dist/Utils/bvhUtils/BvhUtils.d.ts
+++ dist/Utils/bvhUtils/BvhUtils.d.ts
@@ -4,14 +4,12 @@ export declare class BvhManager {
     readonly computeBoundsTree: typeof computeBoundsTree;
     readonly disposeBoundsTree: typeof disposeBoundsTree;
     readonly acceleratedRaycast: typeof acceleratedRaycast;
-    private worker;
-    private _workerPath;
+    private _worker;
     maxDepth: number | undefined;
     constructor();
-    set workerPath(path: URL | undefined);
-    get workerPath(): URL | undefined;
-    set useWorker(bool: boolean);
-    get useWorker(): boolean;
+    set worker(worker: Worker);
+    set useWorker(worker: Worker | null);
+    get useWorker(): Worker | null;
     applyThreeMeshBVH(geometry: BufferGeometry): Promise<void>;
     generate(geometry: BufferGeometry, options?: any): Promise<unknown>;
     dispose(): void;
